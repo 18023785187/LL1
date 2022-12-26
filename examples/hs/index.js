d3d7666481823e5650ec379c40250333939cd4a8
hymmmmmm <1069532844@qq.com>
2022-12-26 21:27:05 +0800

    新增命令行调用

--- examples/hs/index.js
+++ examples/hs/index.js
@@ -3,8 +3,8 @@ const { compiler } = require('./compiler');
 
 fs.readFile('./input.h', 'utf-8', (err, data) => {
   console.log(
-`\x1b[32;1mCompile successfully\x1b[0m
+    `\x1b[32;1mCompile successfully\x1b[0m
 print:`
-    );
+  );
   compiler(data);
 });
